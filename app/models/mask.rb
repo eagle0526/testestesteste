eb2c57f355a147b96d2aa31fb8c134001e1dc3fd
Yee <a034506618@gmail.com>
2023-03-27 03:44:06 +0800

    建立關聯

--- app/models/mask.rb
+++ app/models/mask.rb
@@ -1,2 +1,4 @@
 class Mask < ApplicationRecord
+  has_many :mask_pharmacies
+  has_many :pharmacies, through: :mask_pharmacies
 end
--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -1,2 +1,4 @@
 class Pharmacy < ApplicationRecord
+  has_many :mask_pharmacies
+  has_many :masks, through: :mask_pharmacies
 end
